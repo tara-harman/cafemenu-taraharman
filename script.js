2bf98984edf41f222d225d18374374a4f22451a6
tara-harman <tbharman@icloud.com>
2026-03-03 20:48:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const menuItems = ["Latte", "Espresso", "Green Tea", "Muffin", "Iced Coffee"];
 
+const menuList = document.getElementById("menu-list");
+
+menuItems.forEach(function(item) {
+    const li = document.createElement("li");
+    li.textContent = item;
+    menuList.appendChild(li);
+});
